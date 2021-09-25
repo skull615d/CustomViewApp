a0a91b5035ce48a9a7a7a137de4d30b0d2bce7f5
Igor Fedorov <72979646+igorfedorovme@users.noreply.github.com>
2021-09-26 01:56:12 +0300

    - MLinkedList implemented

--- InnopolisUniversity/AndroidDevelopment/Java/Collections.List/src/main/java/Main.java
+++ InnopolisUniversity/AndroidDevelopment/Java/Collections.List/src/main/java/Main.java
@@ -1,4 +1,5 @@
 import collections.MArrayList;
+import collections.MLinkedList;
 
 import java.util.*;
 
@@ -12,6 +13,13 @@ public class Main {
 
         MArrayList<Integer> mArrayList = new MArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6));
 
+        MLinkedList<Integer> mLinkedList = new MLinkedList<>();
+        mLinkedList.add(1);
+        mLinkedList.add(2);
+        mLinkedList.add(3);
+        mLinkedList.add(4);
+        mLinkedList.add(5);
+        mLinkedList.add(6);
 
         System.out.println("ArrayList: ");
         processList(arrayList);
@@ -24,14 +32,20 @@ public class Main {
         System.out.println("My implementation of ArrayList: ");
         processList(mArrayList);
         System.out.println("---------------------------");
+
+        System.out.println("My implementation of LikedList: ");
+        processList(mLinkedList);
+        System.out.println("---------------------------");
     }
 
     private static void processList(List<Integer> list) {
-        list.forEach(System.out::println);
+//        list.forEach(System.out::println); doesn't work for mLinkedList - Iterator not implemented
+        System.out.println(list.toString());
+        list.add(10);
+        System.out.println("Added 10. List is: " + list);
         System.out.println("Index of 1: " + list.indexOf(1));
         System.out.println("Element 3 removed...");
         list.remove(Integer.valueOf(3));
         System.out.println(list);
     }
-
 }
--- InnopolisUniversity/AndroidDevelopment/Java/Collections.List/src/main/java/collections/MLinkedList.java
+++ InnopolisUniversity/AndroidDevelopment/Java/Collections.List/src/main/java/collections/MLinkedList.java
@@ -1,235 +1,274 @@
 package collections;
 
-import java.util.*;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
 
-public class MLinkedList<E> implements List<E>, Deque<E> {
+public class MLinkedList<E> implements List<E> {
 
-//    private static class Node<E>() {
-//
-//    }
+    private int size = 0;
+    private Node<E> first;
+    private Node<E> last;
 
-    @Override
-    public void addFirst(E e) {
+    public MLinkedList() {
+    }
 
+    Node<E> node(int index) {
+        Node<E> x;
+        // size >> 1 - catchy size / 2
+        if (index < (size >> 1)) {
+            x = first;
+            for (int i = 0; i < index; i++)
+                x = x.nextItem;
+        } else {
+            x = last;
+            for (int i = size - 1; i > index; i--)
+                x = x.previousItem;
+        }
+        return x;
+    }
+
+    private void linkLast(E e) {
+        final Node<E> l = last;
+        final Node<E> newNode = new Node<>(l, e, null);
+        last = newNode;
+        if (l == null)
+            first = newNode;
+        else
+            l.nextItem = newNode;
+        size++;
+    }
+
+    private void linkBefore(E element, Node<E> node) {
+        final Node<E> previousItem = node.previousItem;
+        final Node<E> newNode = new Node<>(previousItem, element, node);
+        node.previousItem = newNode;
+        if (previousItem == null)
+            first = newNode;
+        else
+            previousItem.nextItem = newNode;
+        size++;
+    }
+
+    private E unlink(Node<E> node) {
+        final E item = node.item;
+        final Node<E> nextItem = node.nextItem;
+        final Node<E> previousItem = node.previousItem;
+
+        if (previousItem == null) {
+            first = nextItem;
+        } else {
+            previousItem.nextItem = nextItem;
+            node.previousItem = null;
+        }
+
+        if (nextItem == null) {
+            last = previousItem;
+        } else {
+            nextItem.previousItem = previousItem;
+            node.nextItem = null;
+        }
+
+        node.item = null;
+        size--;
+        return item;
+    }
+
+    private void checkBounds(int index) {
+        if (index >= 0 && index < size)
+            throw new IndexOutOfBoundsException(index);
     }
 
     @Override
-    public void addLast(E e) {
-
-    }
-
-    @Override
-    public boolean offerFirst(E e) {
-        return false;
-    }
-
-    @Override
-    public boolean offerLast(E e) {
-        return false;
-    }
-
-    @Override
-    public E removeFirst() {
-        return null;
-    }
-
-    @Override
-    public E removeLast() {
-        return null;
-    }
-
-    @Override
-    public E pollFirst() {
-        return null;
-    }
-
-    @Override
-    public E pollLast() {
-        return null;
-    }
-
-    @Override
-    public E getFirst() {
-        return null;
-    }
-
-    @Override
-    public E getLast() {
-        return null;
-    }
-
-    @Override
-    public E peekFirst() {
-        return null;
-    }
-
-    @Override
-    public E peekLast() {
-        return null;
-    }
-
-    @Override
-    public boolean removeFirstOccurrence(Object o) {
-        return false;
-    }
-
-    @Override
-    public boolean removeLastOccurrence(Object o) {
-        return false;
-    }
-
-    @Override
-    public boolean offer(E e) {
-        return false;
-    }
-
-    @Override
-    public E remove() {
-        return null;
-    }
-
-    @Override
-    public E poll() {
-        return null;
-    }
-
-    @Override
-    public E element() {
-        return null;
-    }
-
-    @Override
-    public E peek() {
-        return null;
-    }
-
-    @Override
-    public void push(E e) {
-
-    }
-
-    @Override
-    public E pop() {
-        return null;
-    }
-
-    @Override
-    public Iterator<E> descendingIterator() {
-        return null;
+    public String toString() {
+        int max = size - 1;
+        if (max == -1)
+            return "[]";
+        StringBuilder result = new StringBuilder();
+        result.append("[");
+        for (int i = 0; ; i++) {
+            result.append(node(i).item);
+            if (i == max)
+                return result.append("]").toString();
+            result.append(", ");
+        }
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
     @Override
     public boolean contains(Object o) {
-        return false;
-    }
-
-    @Override
-    public Iterator<E> iterator() {
-        return null;
-    }
-
-    @Override
-    public Object[] toArray() {
-        return new Object[0];
-    }
-
-    @Override
-    public <T> T[] toArray(T[] a) {
-        return null;
+        return indexOf(o) >= 0;
     }
 
     @Override
     public boolean add(E e) {
-        return false;
+        linkLast(e);
+        return true;
     }
 
     @Override
     public boolean remove(Object o) {
-        return false;
-    }
-
-    @Override
-    public boolean containsAll(Collection<?> c) {
-        return false;
-    }
-
-    @Override
-    public boolean addAll(Collection<? extends E> c) {
-        return false;
-    }
-
-    @Override
-    public boolean addAll(int index, Collection<? extends E> c) {
-        return false;
-    }
-
-    @Override
-    public boolean removeAll(Collection<?> c) {
-        return false;
-    }
-
-    @Override
-    public boolean retainAll(Collection<?> c) {
+        if (o == null) {
+            for (Node<E> f = first; f != null; f = f.nextItem) {
+                if (f.item == null) {
+                    unlink(f);
+                    return true;
+                }
+            }
+        } else {
+            for (Node<E> f = first; f != null; f = f.nextItem) {
+                if (o.equals(f.item)) {
+                    unlink(f);
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
     @Override
     public void clear() {
-
+        Node<E> node = first;
+        while (node != null) {
+            Node<E> nextItem = node.nextItem;
+            node.item = null;
+            node.previousItem = null;
+            node.nextItem = null;
+            node = nextItem;
+        }
+        first = null;
+        last = null;
+        size = 0;
     }
 
     @Override
     public E get(int index) {
-        return null;
+        checkBounds(index);
+        return node(index).item;
     }
 
     @Override
     public E set(int index, E element) {
-        return null;
+        checkBounds(index);
+        Node<E> node = node(index);
+        E old = node.item;
+        node.item = element;
+        return old;
     }
 
     @Override
     public void add(int index, E element) {
-
+        checkBounds(index);
+        if (index == size)
+            linkLast(element);
+        else
+            linkBefore(element, node(index));
     }
 
     @Override
     public E remove(int index) {
-        return null;
+        checkBounds(index);
+        return unlink(node(index));
     }
 
     @Override
     public int indexOf(Object o) {
-        return 0;
+        int index = 0;
+        if (o == null) {
+            for (Node<E> node = first; node != null; node = node.nextItem) {
+                if (node.item == null)
+                    return index;
+                index++;
+            }
+        } else {
+            for (Node<E> node = first; node != null; node = node.nextItem) {
+                if (o.equals(node.item))
+                    return index;
+                index++;
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public int lastIndexOf(Object o) {
-        return 0;
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends E> c) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends E> c) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public ListIterator<E> listIterator() {
-        return null;
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public ListIterator<E> listIterator(int index) {
-        return null;
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public List<E> subList(int fromIndex, int toIndex) {
-        return null;
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Object[] toArray() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public <T> T[] toArray(T[] a) {
+        throw new UnsupportedOperationException();
+    }
+
+    private static class Node<E> {
+        E item;
+        Node<E> nextItem;
+        Node<E> previousItem;
+
+        Node(Node<E> previousItem, E item, Node<E> nextItem) {
+            this.item = item;
+            this.previousItem = previousItem;
+            this.nextItem = nextItem;
+        }
     }
 }

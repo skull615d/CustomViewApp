c667bd92908bfca12a2d544b3dc57fb12c4e0d78
Igor Fedorov <72979646+igorfedorovme@users.noreply.github.com>
2021-10-06 21:52:45 +0300

    - Started implementing architecture - Started implementing WeatherApi - Added DI (KOIN) - Added dependency for okhttp logging interceptor

--- InnopolisUniversity/AndroidDevelopment/MyApp/app/src/main/java/me/igorfedorov/myapp/MainActivity.kt
+++ InnopolisUniversity/AndroidDevelopment/MyApp/app/src/main/java/me/igorfedorov/myapp/MainActivity.kt
@@ -1,7 +1,6 @@
 package me.igorfedorov.myapp
 
 import android.os.Bundle
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -11,8 +10,20 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val text = mainPresenter.getTemperature()
-        val textView = findViewById<TextView>(R.id.textView)
-        textView.text = text
+//        val text = mainPresenter.getTemperature()
+//        val textView = findViewById<TextView>(R.id.textView)
+//        textView.text = text
+//
+//        val list = listOf(1, 2, 3, 4, 5)
+//
+//        val filteredList: List<Int>? = null
+//
+//        Log.d("TAG", "$filteredList")
+//
+//        filteredList?.let { list ->
+//            textView.text = list.toString()
+//        }
+
+
     }
 }

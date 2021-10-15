bfc686397a4c6fe765e7f552dac6dda582bff78e
Igor Fedorov <72979646+igorfedorovme@users.noreply.github.com>
2021-10-15 23:57:24 +0300

    - Started implementing appModule DI

--- InnopolisUniversity/AndroidDevelopment/NewsFeedApp/app/src/main/java/me/igorfedorov/newsfeedapp/di/Module.kt
+++ InnopolisUniversity/AndroidDevelopment/NewsFeedApp/app/src/main/java/me/igorfedorov/newsfeedapp/di/Module.kt
@@ -1,8 +1,33 @@
 package me.igorfedorov.newsfeedapp.di
 
+import me.igorfedorov.newsfeedapp.BuildConfig
+import me.igorfedorov.newsfeedapp.di.util.ApiKeyInterceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
+private const val BASE_URL = "https://newsapi.org/"
+const val APP_MODULE_OKHTTP = "APP_MODULE_OKHTTP"
+const val APP_MODULE_RETROFIT = "APP_MODULE_RETROFIT"
 val appModule = module {
+
+    single<OkHttpClient>(named(APP_MODULE_OKHTTP)) {
+        OkHttpClient.Builder()
+            .addInterceptor(ApiKeyInterceptor(BuildConfig.NEWS_API_KEY))
+            .addNetworkInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+            .build()
+    }
+
+    single<Retrofit>(named(APP_MODULE_RETROFIT)) {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(get<OkHttpClient>(qualifier = named(APP_MODULE_OKHTTP)))
+            .build()
+    }
 
 
 }
---
+++ InnopolisUniversity/AndroidDevelopment/NewsFeedApp/app/src/main/java/me/igorfedorov/newsfeedapp/di/util/ApiKeyInterceptor.kt
@@ -0,0 +1,15 @@
+package me.igorfedorov.newsfeedapp.di.util
+
+import okhttp3.Interceptor
+import okhttp3.Response
+
+class ApiKeyInterceptor(private val apiKey: String) : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+
+        val modifiedRequest = chain.request().newBuilder()
+            .addHeader("X-Api-Key", apiKey)
+            .build()
+
+        return chain.proceed(modifiedRequest)
+    }
+}

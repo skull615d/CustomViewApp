3c314c8af54bd27664f89c2ad5b55800d8560ca7
Igor Fedorov <72979646+igorfedorovme@users.noreply.github.com>
2021-10-17 23:26:50 +0300

    - Moved init block to top

--- InnopolisUniversity/AndroidDevelopment/NewsFeedApp/app/src/main/java/me/igorfedorov/newsfeedapp/feature/main_screen/ui/adapter/ArticlesAdapter.kt
+++ InnopolisUniversity/AndroidDevelopment/NewsFeedApp/app/src/main/java/me/igorfedorov/newsfeedapp/feature/main_screen/ui/adapter/ArticlesAdapter.kt
@@ -8,6 +8,10 @@ import me.igorfedorov.newsfeedapp.feature.main_screen.domain.model.Article
 
 class ArticleAdapter : AsyncListDifferDelegationAdapter<Article>(ArticleDiffUtilCallback()) {
 
+    init {
+        delegatesManager.addDelegate(articleAdapterDelegate())
+    }
+
     private fun articleAdapterDelegate() =
         adapterDelegateViewBinding<Article, Article, ItemArticleBinding>(
             { layoutInflater, parent -> ItemArticleBinding.inflate(layoutInflater, parent, false) }
@@ -21,11 +25,6 @@ class ArticleAdapter : AsyncListDifferDelegationAdapter<Article>(ArticleDiffUtil
             }
         }
 
-    init {
-        delegatesManager.addDelegate(articleAdapterDelegate())
-    }
-
-
     class ArticleDiffUtilCallback : DiffUtil.ItemCallback<Article>() {
         override fun areItemsTheSame(oldItem: Article, newItem: Article): Boolean {
             return oldItem.url == newItem.url
@@ -34,7 +33,5 @@ class ArticleAdapter : AsyncListDifferDelegationAdapter<Article>(ArticleDiffUtil
         override fun areContentsTheSame(oldItem: Article, newItem: Article): Boolean {
             return oldItem == newItem
         }
-
     }
-
 }

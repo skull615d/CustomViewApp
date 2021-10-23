cb9fe8dd176065b25f130287caddc7fcb539a151
Igor Fedorov <72979646+igorfedorovme@users.noreply.github.com>
2021-10-23 03:14:15 +0300

    - Implemented Custom Tabs :)

--- InnopolisUniversity/AndroidDevelopment/NewsFeedApp/app/src/main/java/me/igorfedorov/newsfeedapp/feature/bookmarks_screen/ui/BookmarksScreenFragment.kt
+++ InnopolisUniversity/AndroidDevelopment/NewsFeedApp/app/src/main/java/me/igorfedorov/newsfeedapp/feature/bookmarks_screen/ui/BookmarksScreenFragment.kt
@@ -1,9 +1,11 @@
 package me.igorfedorov.newsfeedapp.feature.bookmarks_screen.ui
 
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.browser.customtabs.CustomTabsIntent
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -12,6 +14,7 @@ import by.kirich1409.viewbindingdelegate.viewBinding
 import me.igorfedorov.newsfeedapp.R
 import me.igorfedorov.newsfeedapp.base.utils.setAdapterAndCleanupOnDetachFromWindow
 import me.igorfedorov.newsfeedapp.databinding.FragmentBookmarksScreenBinding
+import me.igorfedorov.newsfeedapp.feature.news_feed_screen.domain.model.Article
 import me.igorfedorov.newsfeedapp.feature.news_feed_screen.ui.adapter.ArticlesAdapter
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -22,7 +25,20 @@ class BookmarksScreenFragment : Fragment(R.layout.fragment_bookmarks_screen) {
     private val binding: FragmentBookmarksScreenBinding by viewBinding(createMethod = CreateMethod.INFLATE)
 
     private val articlesAdapter: ArticlesAdapter by lazy {
-        ArticlesAdapter({}, {})
+        ArticlesAdapter(
+            onItemClickListener = {
+                showCustomTab(it)
+            },
+            onBookmarkClick = {
+
+            })
+    }
+
+    private fun showCustomTab(it: Article) {
+        CustomTabsIntent.Builder()
+            .build().apply {
+                launchUrl(requireContext(), Uri.parse(it.url))
+            }
     }
 
     override fun onResume() {

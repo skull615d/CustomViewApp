2de9142a0652c12d1d38a557ab3dcd682fd79c3f
IgorFedorovMe <72979646+IgorFedorovMe@users.noreply.github.com>
2021-10-29 19:39:39 +0300

    - Added dependencies

---
+++ InnopolisUniversity/AndroidDevelopment/CustomViewApp/app/src/main/java/me/igorfedorov/customviewapp/CanvasFragment.kt
@@ -0,0 +1,12 @@
+package me.igorfedorov.customviewapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+
+class CanvasFragment : Fragment(R.layout.fragment_canvas) {
+
+    companion object {
+        fun newInstance() = CanvasFragment()
+    }
+
+}
